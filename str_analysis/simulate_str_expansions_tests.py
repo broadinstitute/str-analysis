ef53937731d740e55533bb6c4e2c2dfc04388744
bw2 <weisburd@broadinistitute.org>
2021-09-15 13:19:47 -0400

    work around cryptic pysam import error

--- str_analysis/simulate_str_expansions_tests.py
+++ str_analysis/simulate_str_expansions_tests.py
@@ -3,7 +3,6 @@ import pyfaidx
 import tempfile
 import unittest
 
-from str_analysis.generate_simulated_bams import generate_synthetic_reference_sequence
 from str_analysis.utils.fasta_utils import get_reference_sequence
 
 
@@ -35,23 +34,6 @@ class Tests(unittest.TestCase):
 
         seq = get_reference_sequence(self.fasta_obj, chrom="chrTest1", start_1based=0, end_1based=1)
         self.assertEqual(seq, "")
-
-    def test_generate_synthetic_reference_sequence(self):
-        seq = generate_synthetic_reference_sequence(
-            self.fasta_obj, chrom="chrTest2", start_1based=5, end_1based=10, padding_length=4, repeat_unit="CAG", num_copies=0)
-        self.assertEqual(seq, "ACGTACGT")
-
-        seq = generate_synthetic_reference_sequence(
-            self.fasta_obj, chrom="chrTest2", start_1based=5, end_1based=10, padding_length=2, repeat_unit="CAG", num_copies=0)
-        self.assertEqual(seq, "GTAC")
-
-        seq = generate_synthetic_reference_sequence(
-            self.fasta_obj, chrom="chrTest2", start_1based=5, end_1based=10, padding_length=2, repeat_unit="CAG", num_copies=3)
-        self.assertEqual(seq, "GTCAGCAGCAGAC")
-
-        seq = generate_synthetic_reference_sequence(
-            self.fasta_obj, chrom="chrTest2", start_1based=5, end_1based=10, padding_length=2, repeat_unit="CAG", num_copies=10)
-        self.assertEqual(seq, "GTCAGCAGCAGCAGCAGCAGCAGCAGCAGCAGAC")
 
     def tearDown(self):
         if os.path.isfile(self.temp_fasta_file.name):
