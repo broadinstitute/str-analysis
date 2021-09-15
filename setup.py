b1d45c024d08011a8e6a75fc86374994e31ca373
bw2 <weisburd@broadinistitute.org>
2021-09-14 20:29:15 -0400

    increment version

--- setup.py
+++ setup.py
@@ -35,7 +35,7 @@ def test_suite():
 
 setup(
     name='str_analysis',
-    version="0.4",
+    version="0.5",
     description="Utilities  short tandem repeats (STRs)",
     install_requires=requirements,
     cmdclass={
