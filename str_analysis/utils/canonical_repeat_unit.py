769042e19da5ce724b421cfd37a18840cd261bfa
bw2 <ben.weisburd@gmail.com>
2021-11-01 18:39:55 -0400

    PR feedback

--- str_analysis/utils/canonical_repeat_unit.py
+++ str_analysis/utils/canonical_repeat_unit.py
@@ -18,7 +18,14 @@ def reverse_complement(dna):
     return "".join([COMPLEMENT[c] for c in dna[::-1]])
 
 
-def _minimal_unit_under_shift(unit):
+def _alphabetically_first_unit_under_shift(unit):
+    """Returns the permutation of the given repeat unit that's first alphabetically.
+
+    Args:
+        unit (str): A repeat unit like "CAG".
+    Return:
+        str: The alphabetically first repeat unit.
+    """
     minimal_unit = unit
     double_unit = unit + unit
     for i in range(len(unit)):
@@ -32,11 +39,16 @@ def compute_canonical_repeat_unit(unit):
     """Takes an STR motif string like "GAA" and returns the "canonical" representation of the motif. This is the
     rearrangement of the motif bases (including reverse complements) that is alphabetically first. For "GAA", this would
     return "AAG" since it's alphabetically first among "GAA", "AGA", "AAG", "TTC", "TCT", and "CTT"
+
+    Args:
+        unit (str): A repeat unit like "CAG".
+    Return:
+        str: The alphabetically first repeat unit.
     """
-    minimal_unit = _minimal_unit_under_shift(unit)
+    minimal_unit = _alphabetically_first_unit_under_shift(unit)
 
     unit_rc = reverse_complement(unit)
-    minimal_unit_rc = _minimal_unit_under_shift(unit_rc)
+    minimal_unit_rc = _alphabetically_first_unit_under_shift(unit_rc)
 
     if minimal_unit_rc < minimal_unit:
         return minimal_unit_rc
