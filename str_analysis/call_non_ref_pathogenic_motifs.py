a221362d19191d7c23418e738e1d15e0a4cdbe83
bw2 <bw2@users.noreply.github.com>
2021-11-10 12:13:29 -0500

    Update str_analysis/call_non_ref_pathogenic_motifs.py
    Co-authored-by: Katherine Chao <kchao@broadinstitute.org>

--- str_analysis/call_non_ref_pathogenic_motifs.py
+++ str_analysis/call_non_ref_pathogenic_motifs.py
@@ -218,7 +218,7 @@ def run_expansion_hunter(
     use_offtarget_regions=False,
 ):
     """
-    Run ExpansionHunter and parse relevant output fields + add them to results.
+    Run ExpansionHunter, parse relevant output fields, and add them to results.
     Then optionally run REViewer to generate read visualizations.
 
     Args:
