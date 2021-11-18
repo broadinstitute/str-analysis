9f80f8bd0a5e7efff507294a0e198a1b7244ada0
bw2 <ben.weisburd@gmail.com>
2021-11-17 21:38:26 -0500

    more PR feedback

---
+++ str_analysis/utils/export_json.py
@@ -0,0 +1,26 @@
+import gzip
+import json
+import os
+
+
+def export_json(json_data, local_output_path, google_storage_dir=None):
+    """Utility function for writing a json data structure to a file.
+
+    Args:
+        json_data (dict or list): The .json structure to write out.
+        local_output_path (str): Local output path where to write the json data.
+        google_storage_dir (str): Optional "gs://" directory path where to copy the local json file after it's written.
+    """
+
+    if google_storage_dir and not google_storage_dir.startswith("gs://"):
+        raise ValueError(f"Google Storage dir ({google_storage_dir}) doesn't start with 'gs://'")
+
+    open_file = gzip.open if local_output_path.endswith(".gz") else open
+
+    print(f"Writing {local_output_path}")
+    with open_file(local_output_path, "wt") as f:
+        json.dump(json_data, f, indent=3, ensure_ascii=True, allow_nan=False)
+
+    google_storage_path = os.path.join(google_storage_dir, os.path.basename(local_output_path))
+    print(f"Copying {local_output_path} to {google_storage_path}")
+    os.system(f"gsutil -m cp {local_output_path} {google_storage_path}")
