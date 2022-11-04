14ed8fb996ec36ba69dc14800023c065be0dd501
bw2 <ben.weisburd@gmail.com>
2022-11-04 17:29:07 -0400

    update version

--- setup.py
+++ setup.py
@@ -40,7 +40,7 @@ def test_suite():
 
 setup(
     name='str_analysis',
-    version="0.9.10",
+    version="0.9.12",
     description="Utilities  short tandem repeats (STRs)",
     install_requires=requirements,
     cmdclass={
