583ca25e4ea975e70f5dd5976578ba1cca3e739c
bw2 <ben.weisburd@gmail.com>
2023-03-16 10:18:31 -0400

    added --discard-loci-with-multiple-indels

--- str_analysis/filter_vcf_to_STR_variants.py
+++ str_analysis/filter_vcf_to_STR_variants.py
@@ -66,13 +66,18 @@ def parse_args():
     p.add_argument("--min-str-repeats", type=int, default=3, help="Minimum STR size in number of repeats. This "
                    "threshold applies to the total repeat length comprising any repeats in the flanking sequence to "
                    "the left and right of the variant + in the inserted or deleted bases themselves")
+
     p.add_argument("--allow-interruptions", help="Whether to allow interruptions in the repeat sequence.", action="store_true")
+    p.add_argument("--discard-loci-with-multiple-indels", help="Sometimes a VCF will have multiple INDEL variants within the same STR locus. "
+                   "This occurs more often within interrupted repeats since left alignment of INDELs doesn't work as well there."
+                   "If this flag is used, STR loci that contain more than one INDEL variant will be filtered out.", action="store_true")
+
     p.add_argument("--show-progress-bar", help="Show a progress bar in the terminal when processing variants.", action="store_true")
     p.add_argument("-n", type=int, help="Only process the first N rows of the VCF. Useful for testing.")
     p.add_argument("-o", "--output-prefix", help="Output vcf prefix. If not specified, it will be computed based on "
                                                  "the input vcf filename")
     p.add_argument("--write-bed-file", help="Whether to output a .bed file containing the STR variants. This requires "
-                   "bgzip and tabix tools to be available in the shell environment.", action="store_true")
+                   "bedtools, bgzip and tabix tools to be available in the shell environment.", action="store_true")
     p.add_argument("-L", "--interval", help="Only process variants in this genomic interval (format: chrN:start-end)", action="append")
     p.add_argument("input_vcf_path")
 
@@ -255,7 +260,7 @@ def check_if_allele_is_str(
         "NumRepeatsLeftFlank": num_total_repeats_left_flank,
         "NumRepeatsRightFlank": num_total_repeats_right_flank,
         "NumRepeatsInVariant": num_total_repeats_within_variant_bases,
-        "IsPureRepeat": "Yes" if is_pure_repeat else "No",
+        "IsPureRepeat": is_pure_repeat,
         "FractionPureRepeats": num_pure_repeats_in_str / num_total_repeats_in_str,
         "RepeatUnitInterruptionIndex": repeat_unit_interruption_index,
         "PureStart1Based": pure_start_1based,
@@ -361,26 +366,29 @@ def compute_variant_summary_string(alt_STR_allele_specs, het_or_hom):
     ])
     summary_string += ":" + het_or_hom
 
-    if alt_STR_allele_specs[0]["IsPureRepeat"] != "Yes":
+    if not alt_STR_allele_specs[0]["IsPureRepeat"]:
         summary_string += ":not-pure"
 
     return ":".join(ins_or_del), summary_string
 
 
-def process_truth_set_vcf_line(
+def process_vcf_line(
         vcf_line_i,
         vcf_fields,
         fasta_obj,
         vcf_writer,
         variants_tsv_writer,
         alleles_tsv_writer,
-        variants_bed_records,
+        bed_writer,
         args,
-        counters):
+        counters,
+        indels_per_locus_counter,
+):
     """Utility method for processing a single line from the input vcf"""
 
     # parse the ALT allele(s)
     vcf_chrom = vcf_fields[0]
+    vcf_chrom_without_chr_prefix = vcf_chrom.replace("chr", "")
     vcf_pos = int(vcf_fields[1])
     vcf_ref = vcf_fields[3]
     vcf_alt = vcf_fields[4]
@@ -389,7 +397,7 @@ def process_truth_set_vcf_line(
         counters[f"ERROR: no alt allele in VCF row"] += 1
         return
 
-    variant_id = f"{vcf_chrom}-{vcf_pos}-{vcf_ref}-{vcf_alt}"
+    variant_id = f"{vcf_chrom_without_chr_prefix}-{vcf_pos}-{vcf_ref}-{vcf_alt}"
 
     alt_alleles = vcf_alt.split(",")
     if len(alt_alleles) > 2:
@@ -469,8 +477,7 @@ def process_truth_set_vcf_line(
         return
 
     if len(alt_STR_allele_specs) > 1 and any(allele_spec is None for allele_spec in alt_STR_allele_specs):
-        # this truth set is intended for benchmarking downstream tools that only call STR variants, so
-        # multiallelics with, for example, one SNV allele and one STR allele will cause problems downstream.
+        # multiallelics with one non-STR allele (such as an SNV) and one STR allele are difficult to process downstream.
         # There are very few of these, so just discard them.
         counters[f"skipped variant: mixed STR/non-STR multi-allelic variant"] += 1
         return
@@ -516,11 +523,15 @@ def process_truth_set_vcf_line(
 
     counters["STR variant counts: TOTAL"] += 1
 
+    locus_id = f"{vcf_chrom_without_chr_prefix}-{start_1based - 1}-{end_1based}-{repeat_unit}"
+    indels_per_locus_counter[locus_id] += 1
+
     # output the vcf record
     INFO_field = ";".join([
         f"Motif={repeat_unit}",
         f"NumRepeats1={num_repeats_in_allele1}",
         f"NumRepeats2={num_repeats_in_allele2}",
+        f"LocusId={locus_id}",
     ])
     vcf_fields[2] = variant_summary_string
     vcf_fields[4] = ",".join(alt_alleles)
@@ -536,8 +547,8 @@ def process_truth_set_vcf_line(
         "Chrom": vcf_chrom,
         "Start1Based": start_1based,
         "End1Based": end_1based,
-        "Locus": f"{vcf_chrom}:{start_1based}-{end_1based}",
-        "LocusId": f"{vcf_chrom}-{start_1based - 1}-{end_1based}-{repeat_unit}",
+        "Locus": f"{vcf_chrom_without_chr_prefix}:{start_1based}-{end_1based}",
+        "LocusId": locus_id,
         "NumRepeatsInReference": (end_1based - start_1based + 1)/len(repeat_unit),
         "Motif": repeat_unit,
         "CanonicalMotif": compute_canonical_motif(repeat_unit, include_reverse_complement=True),
@@ -547,15 +558,15 @@ def process_truth_set_vcf_line(
         "VcfGenotype": vcf_genotype,
         "HET_or_HOM": het_or_hom,
         "IsPureRepeat": is_pure_repeat,
-        "IsMultiallelic": "Yes" if len(alt_alleles) > 1 else "No",
-        "IsFoundInReference": "Yes" if any(found_in_reference(spec) for spec in alt_STR_allele_specs) else "No",
+        "IsMultiallelic": len(alt_alleles) > 1,
+        "IsFoundInReference": any(found_in_reference(spec) for spec in alt_STR_allele_specs),
     }
 
     if variant_short_allele_size < 0 or variant_long_allele_size < 0:
         raise ValueError(f"Short or long allele size is < 0: "
                          f"{variant_short_allele_size}, {variant_long_allele_size}  {pformat(tsv_record)}")
 
-    counters[f"STR variant counts: pure repeats"] += 1 if is_pure_repeat == "Yes" else 0
+    counters[f"STR variant counts: pure repeats"] += 1 if is_pure_repeat else 0
 
     variant_tsv_record = dict(tsv_record)
     variant_tsv_record.update({
@@ -568,9 +579,9 @@ def process_truth_set_vcf_line(
         "RepeatSizeLongAllele (bp)": variant_long_allele_size * len(repeat_unit),
     })
     variants_tsv_writer.write("\t".join([str(variant_tsv_record[c]) for c in VARIANT_TSV_OUTPUT_COLUMNS]) + "\n")
-    if args.write_bed_file:
-        variants_bed_records.add(
-            (vcf_chrom, start_1based - 1, end_1based, variant_summary_string))
+
+    if bed_writer is not None:
+        bed_writer.write("\t".join(map(str, [vcf_chrom, start_1based - 1, end_1based, variant_summary_string, "."])) + "\n")
 
     for alt_allele, alt_STR_allele_spec in zip(alt_alleles, alt_STR_allele_specs):
         allele_tsv_record = dict(tsv_record)
@@ -597,6 +608,7 @@ def main():
         args.output_prefix = re.sub(".vcf(.gz)?", "", os.path.basename(args.input_vcf_path)) + ".STRs"
 
     counters = collections.defaultdict(int)
+    indels_per_locus_counter = collections.defaultdict(int)
 
     # open the reference genome fasta
     fasta_obj = pyfaidx.Fasta(args.reference_fasta_path, one_based_attributes=False, as_raw=True)
@@ -605,11 +617,11 @@ def main():
     fopen = gzip.open if args.input_vcf_path.endswith("gz") else open
     vcf_writer = open(f"{args.output_prefix}.vcf", "wt")
 
-    variants_bed_records = set()
     variants_tsv_writer = open(f"{args.output_prefix}.variants.tsv", "wt")
     variants_tsv_writer.write("\t".join(VARIANT_TSV_OUTPUT_COLUMNS) + "\n")
     alleles_tsv_writer = open(f"{args.output_prefix}.alleles.tsv", "wt")
     alleles_tsv_writer.write("\t".join(ALLELE_TSV_OUTPUT_COLUMNS) + "\n")
+    bed_writer = open(f"{args.output_prefix}.variants.bed", "wt") if args.write_bed_file else None
 
     # open the input VCF
     vcf_iterator = fopen(args.input_vcf_path, "rt")
@@ -638,14 +650,25 @@ def main():
             break
         vcf_line_i += 1
 
-        process_truth_set_vcf_line(vcf_line_i, vcf_fields, fasta_obj,
-            vcf_writer, variants_tsv_writer, alleles_tsv_writer, variants_bed_records, args, counters)
+        process_vcf_line(vcf_line_i, vcf_fields, fasta_obj,
+            vcf_writer, variants_tsv_writer, alleles_tsv_writer, bed_writer, args, counters, indels_per_locus_counter)
 
-    vcf_writer.close()
-    variants_tsv_writer.close()
-    alleles_tsv_writer.close()
-    print("Finished writing to ", variants_tsv_writer.name)
-    print("Finished writing to ", alleles_tsv_writer.name)
+    locus_ids_with_multiple_indels = set()
+    for locus_id, count in indels_per_locus_counter.items():
+        if count > 1:
+            print(f"WARNING: {locus_id} locus contained {count} different INDEL variants in the input vcf")
+            locus_ids_with_multiple_indels.add(locus_id)
+
+    for writer in vcf_writer, variants_tsv_writer, alleles_tsv_writer, bed_writer:
+        if writer is None:
+            continue
+        writer.close()
+        if args.discard_loci_with_multiple_indels and len(locus_ids_with_multiple_indels) > 0:
+            print(f"Filtering {len(locus_ids_with_multiple_indels)} loci from {writer.name} because they have "
+                  f"more than one STR variant")
+            discard_loci_with_multiple_indels(writer.name, locus_ids_with_multiple_indels)
+
+        print("Finished writing to", writer.name)
 
     print_stats(counters)
 
@@ -657,7 +680,54 @@ def main():
     os.system(f"tabix -f {args.output_prefix}.vcf.gz")
 
     if args.write_bed_file:
-        write_bed_file(variants_bed_records, f"{args.output_prefix}.variants.bed")
+        os.system(f"bedtools sort -i {bed_writer.name} | bgzip > {bed_writer.name}.gz")
+        os.system(f"tabix -f {bed_writer.name}.gz")
+
+
+def discard_loci_with_multiple_indels(file_path, locus_ids_with_multiple_indels):
+    if not any(file_path.endswith(suffix) for suffix in (".bed", ".vcf", ".tsv")):
+        raise ValueError(f"Unexpected file type: {file_path}")
+
+    filtered_count = total = 0
+    with open(file_path, "rt") as f, open(f"{file_path}.temp", "wt") as fo:
+        header = None
+        for line in f:
+            if file_path.endswith(".vcf") and line.startswith("#"):
+                fo.write(line)
+                continue
+            if file_path.endswith(".tsv") and header is None:
+                header = line.strip().split("\t")
+                if "LocusId" not in header:
+                    raise ValueError(f"LocusId column not found in {file_path} header: {header}")
+                continue
+
+            total += 1
+            fields = line.strip().split("\t")
+            if file_path.endswith(".tsv"):
+                row = dict(zip(header, fields))
+                locus_id = row["LocusId"]
+            elif file_path.endswith(".vcf"):
+                info = dict([info_record.split("=") for info_record in fields[7].split(";")])
+                if "LocusId" not in info:
+                    raise ValueError(f"Unexpected info field format in {file_path} line: {fields}")
+                locus_id = info["LocusId"]
+            elif file_path.endswith(".bed"):
+                chrom = fields[0].replace("chr", "")
+                summary_string_fields = fields[3].split(":")
+                if len(summary_string_fields) < 2:
+                    raise ValueError(f"Unexpected summary string format in {file_path} line: {fields}")
+                motif = summary_string_fields[1]
+                locus_id = f"{chrom}-{fields[1]}-{fields[2]}-{motif}"
+
+            if locus_id in locus_ids_with_multiple_indels:
+                filtered_count += 1
+                continue
+
+            fo.write(line)
+
+    os.rename(f"{file_path}.temp", file_path)
+
+    print(f"Discarded {filtered_count} out of {total} rows from {file_path} as loci that contained more than one STR variant")
 
 
 def print_stats(counters):
@@ -684,18 +754,6 @@ def print_stats(counters):
             percent = f"{100*value / total:5.1f}%" if total > 0 else ""
 
             print(f"{value:10,d} out of {total:10,d} ({percent}) {key}")
-
-
-def write_bed_file(variants_bed_records, output_bed_path):
-    """Write truth set records to a bed file"""
-    with open(output_bed_path, "wt") as f:
-        for chrom, start_0based, end_1based, name in sorted(variants_bed_records):
-            f.write("\t".join(map(str, [chrom, start_0based, end_1based, name, "."])) + "\n")
-
-    os.system(f"bgzip -f {output_bed_path}")
-    os.system(f"tabix -f {output_bed_path}.gz")
-
-    print(f"Wrote {len(variants_bed_records)} to {output_bed_path}.gz")
 
 
 if __name__ == "__main__":
