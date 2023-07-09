c788ace2dba2e7faab174ca8c3c0b790eaaaea4c
bw2 <ben.weisburd@gmail.com>
2023-07-09 17:48:16 -0400

    added download_local_copy(url_or_google_storage_path)

--- str_analysis/utils/file_utils.py
+++ str_analysis/utils/file_utils.py
@@ -1,11 +1,15 @@
 import gzip
+import hailtop.fs as hfs
 import io
 import os
+import requests
+import tempfile
 
-import hailtop.fs as hfs
 
+def open_file(path, download_local_copy_before_opening=False):
+    if path.startswith("gs://") and download_local_copy_before_opening:
+        path = download_local_copy(path)
 
-def open_file(path):
     path = os.path.expanduser(path)
     mode = "r"
     if path.startswith("gs://"):
@@ -28,3 +32,25 @@ def file_exists(path):
 
     path = os.path.expanduser(path)
     return os.path.isfile(path)
+
+
+def download_local_copy(url_or_google_storage_path):
+    """Downloads the given URL or gs:// path to a local temp file and returns the path to the local file."""
+
+    temp_dir = tempfile.gettempdir()
+    if url_or_google_storage_path.startswith("gs://"):
+        path = os.path.join(temp_dir, os.path.basename(url_or_google_storage_path))
+        if not os.path.isfile(path):
+            print(f"Downloading {url_or_google_storage_path} to {path}")
+            hfs.copy(url_or_google_storage_path, path)
+    else:
+        path = os.path.join(temp_dir, os.path.basename(url_or_google_storage_path))
+        if not os.path.isfile(path):
+            print(f"Downloading {url_or_google_storage_path} to {path}")
+            r = requests.get(url_or_google_storage_path)
+            with open(path, "wb") as f:
+                f.write(r.content)
+
+    return path
+
+
