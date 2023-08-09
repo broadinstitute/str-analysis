f13328dbc4c3e988d1db7a72c4a9e70271f56210
bw2 <ben.weisburd@gmail.com>
2023-08-09 11:42:40 -0400

    added max_adjacent_repeats arg

--- str_analysis/utils/get_adjacent_repeats.py
+++ str_analysis/utils/get_adjacent_repeats.py
@@ -6,7 +6,7 @@ from str_analysis.utils.misc_utils import parse_interval
 # allow up to 15bp of reference sequence between adjacent repeats
 MAX_DISTANCE_BETWEEN_REPEATS = 15
 
-# look for adjacent intervals that are no more than 1000bp to the left or right of the main repeat.
+# look for adjacent repeats that are no more than 1000bp to the left or right of the main repeat.
 MAX_TOTAL_ADJACENT_REGION_SIZE = 1000
 
 # TandemRepeatFinder can output repeat stretch that overlap by several base pairs - for example:
@@ -16,6 +16,8 @@ MAX_OVERLAP_BETWEEN_ADJACENT_REPEATS = 3
 
 
 def get_repeat_unit_from_fasta(chrom, start_1based, end_1based, repeat_unit_length, pysam_fasta_file):
+    """Get the reference sequence from the genome fasta file at the given coordinates."""
+
     ref_fasta_sequence = pysam_fasta_file.fetch(chrom, start_1based - 1, end_1based)  # fetch uses 0-based coords
     repeat_unit = ref_fasta_sequence[:repeat_unit_length]  # get repeat unit from reference that matches these coords
     repeat_unit = repeat_unit.upper()  # convert to upper-case since the reference contains some lower-case regions
@@ -31,7 +33,25 @@ def get_adjacent_repeats(locus_interval_0based, repeat_unit, pysam_fasta_file, i
     max_distance_between_adjacent_repeats=MAX_DISTANCE_BETWEEN_REPEATS,
     max_total_adjacent_region_size=MAX_TOTAL_ADJACENT_REGION_SIZE,
     max_overlap_between_adjacent_repeats=MAX_OVERLAP_BETWEEN_ADJACENT_REPEATS,
+    max_adjacent_repeats=None,
 ):
+    """Find adjacent repeats on the left and right of the given locus interval.
+
+    Args:
+        locus_interval_0based (str): locus interval in 0-based format, e.g. "chr1:100-200"
+        repeat_unit (str): repeat unit
+        pysam_fasta_file (pysam.FastaFile): pysam FastaFile object
+        interval_tree (intervaltree.IntervalTree): an IntervalTree containing 0-based intervals that represent the
+            reference start and end coordinates of all TR loci found on the same chromosome as the given record.
+            Adjacent repeats will be queried from this data structure.
+        max_distance_between_adjacent_repeats (int): maximum distance between adjacent repeats
+        max_total_adjacent_region_size (int): maximum total size of the region to search for adjacent repeats
+        max_overlap_between_adjacent_repeats (int): maximum overlap between adjacent repeats
+        max_adjacent_repeats (int): maximum number of adjacent repeats to return
+
+    Returns:
+         3-tuple: (adjacent_repeats_on_left, locus_structure, adjacent_repeats_on_right)
+    """
     chrom, start_0based, end_1based = parse_interval(locus_interval_0based)
 
     repeat_units_already_added = set()
@@ -87,6 +107,9 @@ def get_adjacent_repeats(locus_interval_0based, repeat_unit, pysam_fasta_file, i
             chrom, adj_repeat.start_1based - 1, adj_repeat.end_1based, repeat_unit)
         adjacent_repeats_on_left.append(adj_repeat_left_string)
 
+        if max_adjacent_repeats and len(adjacent_repeats_on_left) >= max_adjacent_repeats:
+            break
+
         # move current_left_coord to the start_1based position of this next repeat on the left.
         current_left_coord_1based = adj_repeat.start_1based
         locus_structure = f"({repeat_unit})*{ref_spacer}{locus_structure}"
@@ -138,6 +161,9 @@ def get_adjacent_repeats(locus_interval_0based, repeat_unit, pysam_fasta_file, i
         adj_repeat_right_string = compute_locus_id(
             chrom, adj_repeat.start_1based - 1, adj_repeat.end_1based, repeat_unit)
         adjacent_repeats_on_right.append(adj_repeat_right_string)
+
+        if max_adjacent_repeats and len(adjacent_repeats_on_right) >= max_adjacent_repeats:
+            break
 
         current_right_coord_1based = adj_repeat.end_1based
         locus_structure = f"{locus_structure}{ref_spacer}({repeat_unit})*"
