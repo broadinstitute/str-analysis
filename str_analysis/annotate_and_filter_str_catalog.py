98068c2700b191554486a4f3cef8b98092bc3ec6
bw2 <ben.weisburd@gmail.com>
2023-08-10 22:42:25 -0400

    fix bug where 'ReferenceRegion' fields had a 1-based start coordinate instead of 0-based as required by ExpansionHunter

--- str_analysis/annotate_and_filter_str_catalog.py
+++ str_analysis/annotate_and_filter_str_catalog.py
@@ -168,7 +168,7 @@ def get_variant_catalog_iterator(variant_catalog_json_or_bed):
                 motif = fields[3].strip("()*+")
                 record = {
                     "LocusId": f"{chrom}-{start_0based + 1}-{end_1based}-{motif}",
-                    "ReferenceRegion": f"{unmodified_chrom}:{start_0based + 1}-{end_1based}",
+                    "ReferenceRegion": f"{unmodified_chrom}:{start_0based}-{end_1based}",
                     "LocusStructure": f"({motif})*",
                     "VariantType": "Repeat",
                 }
