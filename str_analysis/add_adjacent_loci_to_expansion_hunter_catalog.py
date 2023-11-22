05696c08c399ce4ed12f26e2c0b82e4cd6d57900
bw2 <ben.weisburd@gmail.com>
2023-11-21 23:00:01 -0500

    implemented -x/--dont-add-adjacent-loci-to-this-locus-id arg, switched to the same naming convention as the official illumina catalog for adjacent variant ids (eg. 'FXN_A' for the poly-A repeat next to FXN), added sort at the end to restore the same sort order as in the input catalog

--- str_analysis/add_adjacent_loci_to_expansion_hunter_catalog.py
+++ str_analysis/add_adjacent_loci_to_expansion_hunter_catalog.py
@@ -32,6 +32,7 @@ def process_input_record(
     max_adjacent_repeats=None,
     add_extra_info_to_locus_id=False,
     add_extra_fields=False,
+    set_variant_ids_to_locus_id_plus_motif=True,
 ):
     """Add adjacent loci to a record from the variant catalog
 
@@ -51,6 +52,10 @@ def process_input_record(
         add_extra_info_to_locus_id (bool) If True, add info about adjacent loci to the locus id in the output variant
         add_extra_fields (bool) If True, add extra fields to the output record that record the number of base pairs
             between the main repeat and any adjacent repeats.
+        set_variant_ids_to_locus_id_plus_motif (bool) If True, set the variant id of adjacent loci to the locus id
+            followed by the motif. This matches the naming convention in the official Illumina catalog. For
+            example: FXN_A is the adjacent poly-A repeat next to the main FXN locus. If False, use a longer, more
+            descriptive variant id like "FXN_ADJACENT_LEFT_1_A".
 
     Return:
         dict: An copy of the input record, modified to include any adjacent repeats found in the interval_tree.
@@ -87,14 +92,20 @@ def process_input_record(
             distances_between_reference_regions = []
             reference_region_motifs = []
 
-        for adjacent_repeat_label, adjacent_repeat in [
+        for variant_id, adjacent_repeat in [
             (f"{locus_id}_ADJACENT_LEFT_{i+1}", adjacent_repeat_left) for i, adjacent_repeat_left in enumerate(adjacent_repeats_left)
         ] + [(locus_id, f"{chrom}:{start_0based}-{end_1based}-{repeat_unit}")] + [
             (f"{locus_id}_ADJACENT_RIGHT_{i+1}", adjacent_repeat_right) for i, adjacent_repeat_right in enumerate(adjacent_repeats_right)
         ]:
             adjacent_repeat_chrom, adjacent_repeat_start_0based, adjacent_repeat_end_1based, adjacent_repeat_unit = re.split("[:-]", adjacent_repeat)
             reference_regions.append(f"{adjacent_repeat_chrom}:{adjacent_repeat_start_0based}-{adjacent_repeat_end_1based}")
-            variant_ids.append(adjacent_repeat_label)
+            if variant_id == locus_id:
+                variant_ids.append(locus_id)
+            elif set_variant_ids_to_locus_id_plus_motif:
+                variant_ids.append(f"{locus_id}_{adjacent_repeat_unit}")
+            else:
+                variant_ids.append(f"{variant_id}_{adjacent_repeat_unit}")
+
             variant_types.append("Repeat")
             if add_extra_info_to_locus_id or add_extra_fields:
                 reference_region_motifs.append(adjacent_repeat_unit)
@@ -250,6 +261,9 @@ def main():
                         action="store_true",
                         help="Add extra fields to each locus in the output variant catalog that record the number of "
                              "base pairs between any adjacent loci and the main locus in an easy-to-parse format")
+    parser.add_argument("-x", "--dont-add-adjacent-loci-to-this-locus-id", action="append", help="Don't compute "
+                        "adjacent repeats for the given locus id, and instead just add it to the output catalog "
+                        "without modification. This option can be specified more than once.")
     parser.add_argument("--output-dir",
                         help="Output directory. If not specified, the input catalog's directory will be used.")
     parser.add_argument("-o", "--output-catalog",
@@ -271,7 +285,7 @@ def main():
     ref_fasta_chromosomes = set(ref_fasta.references)
 
     # parse the input catalog(s)
-    input_catalogs = []
+    input_catalogs = collections.defaultdict(dict)
     for input_catalog_path in args.input_catalog_paths:
         print(f"Loading input catalog {input_catalog_path}")
         with open_file(input_catalog_path) as f:
@@ -284,20 +298,24 @@ def main():
             else:
                 input_catalog = json.load(f)
 
+        input_catalogs[input_catalog_path]["original_locus_id_sort_order"] = [
+            record["LocusId"] for record in input_catalog]
+
         # sort the input catalog by its ReferenceRegion chromosome
         input_catalog.sort(key=lambda record: (
             record["ReferenceRegion"][0].split(":")[0]
             if isinstance(record["ReferenceRegion"], list) else
             record["ReferenceRegion"].split(":")[0]))
 
-        input_catalogs.append(input_catalog)
+        input_catalogs[input_catalog_path]["input_catalog_records"] = input_catalog
+        print(f"Loaded {len(input_catalog):,d} records from {input_catalog_path}")
 
     print(f"Processing {len(input_catalogs)} input variant catalog" + ("s" if len(input_catalogs) > 1 else f": {args.input_catalog_paths[0]}"))
 
     # validate input catalog records and get the min and max coordinates of the TR loci for each chromosome
     min_max_coords_for_chrom = collections.defaultdict(lambda: (10**10, 0))
-    for input_catalog in input_catalogs:
-        for record_i, record in enumerate(input_catalog):
+    for input_catalog in input_catalogs.values():
+        for record_i, record in enumerate(input_catalog["input_catalog_records"]):
             missing_keys = {"ReferenceRegion", "LocusStructure", "LocusId"} - set(record.keys())
             if missing_keys:
                 print(f"WARNING: record {record} is missing keys: {missing_keys}. Skipping...")
@@ -335,18 +353,29 @@ def main():
 
     # process the records from the input catalog(s) to add adjacent loci
     counters = collections.defaultdict(int)
-    for input_catalog_path, input_catalog in zip(args.input_catalog_paths, input_catalogs):
+    locus_ids_with_existing_adjacent_repeats = []
+    locus_ids_with_new_adjacent_repeats = []
+    for input_catalog_path, input_catalog in input_catalogs.items():
         output_catalog = []
         already_added_to_output_catalog = set()  # avoid duplicate records in the output catalog
-        for record_i, record in enumerate(input_catalog):
+        for record_i, record in enumerate(input_catalog["input_catalog_records"]):
             if args.n and record_i >= args.n:
                 break
             counters["total records"] += 1
-            # handle input catalog recordsd that already have adjacent loci
+
+            if args.dont_add_adjacent_loci_to_this_locus_id and record["LocusId"] in args.dont_add_adjacent_loci_to_this_locus_id:
+                print(f"Adding {record['LocusId']} without modification as requested by -x arg")
+                output_catalog.append(record)
+                counters["excluded locus id"] += 1
+                continue
+
+            # handle input catalog records that already have adjacent loci
             if isinstance(record["ReferenceRegion"], list):
                 print(f"WARNING: record {record} already has adjacent intervals specified in the input variant catalog. "
                       f"Will add it to the output variant catalog without modification...")
                 output_catalog.append(record)
+                counters["already had adjacent loci"] += 1
+                locus_ids_with_existing_adjacent_repeats.append(record["LocusId"])
                 continue
 
             # add any adjacent loci to the current record
@@ -365,6 +394,7 @@ def main():
 
             # check if adjacent loci were added
             if isinstance(output_record["ReferenceRegion"], list):
+                locus_ids_with_new_adjacent_repeats.append(output_record["LocusId"])
                 counters["added adjacent loci"] += 1
                 counters["total reference regions"] += len(output_record["ReferenceRegion"])
                 counters["total spacers"] += len(output_record["ReferenceRegion"]) - 1
@@ -373,7 +403,7 @@ def main():
                 output_record_key = output_record["LocusStructure"] + ";" + output_record["ReferenceRegion"]
                 counters["total reference regions"] += 1
 
-            # if this isn't a duplicate record, add it to the output catalog
+            # if this is not a duplicate record, add it to the output catalog
             if output_record_key not in already_added_to_output_catalog:
                 already_added_to_output_catalog.add(output_record_key)
                 output_catalog.append(output_record)
@@ -402,33 +432,47 @@ def main():
         if args.output_dir:
             output_catalog_path = os.path.join(args.output_dir, os.path.basename(output_catalog_path))
 
+        # restore original sort order to match  loci in the input catalog
+        output_catalog.sort(key=lambda record:
+            input_catalog["original_locus_id_sort_order"].index(record["LocusId"]))
+
         fopen = gzip.open if output_catalog_path.endswith("gz") else open
         with fopen(output_catalog_path, "wt") as f:
-            json.dump(output_catalog, f, indent=1)
+            json.dump(output_catalog, f, indent=4)
+
+        if counters["already had adjacent loci"]:
+            print(f"Added {counters['already had adjacent loci']:,d} out of {counters['total records']:,d} records "
+                  f"to the output catalog without modification since they already had adjacent repeats provided in the "
+                  f"input catalog: ",
+                  ", ".join(locus_ids_with_existing_adjacent_repeats[:10]),
+                  "" if len(locus_ids_with_existing_adjacent_repeats) <= 10 else "...")
+        else:
+            print("None of the records in the input catalog(s) had adjacent loci specified")
+
+        print(f"Added adjacent loci to {counters['added adjacent loci']:,d} out of {counters['total records']:,d} records ("
+              f"{counters['added adjacent loci'] / counters['total records']:.1%}): ",
+              ", ".join(locus_ids_with_new_adjacent_repeats[:10]),
+              "" if len(locus_ids_with_new_adjacent_repeats) <= 10 else "...")
+        if args.dont_add_adjacent_loci_to_this_locus_id:
+            print(f"Added {counters['excluded locus id']:,d} out of {counters['total records']:,d} records ("
+                  f"{counters['excluded locus id'] / counters['total records']:.1%}) without modification as "
+                  f"requested by the -x arg")
+
+        for key, value in sorted(counters.items(), key=lambda x: x[0]):
+            if key.startswith("total") or key in {"already had adjacent loci", "added adjacent loci"}:
+                continue
+            if "adjacent repeats count" in key:
+                label = "reference regions"
+            elif "spacer" in key:
+                label = "spacers"
+            else:
+                continue
+
+            total = counters[f"total {label}"]
+            print(f"{value:8,d} out of {total:8,d} {label} ({value / total:6.1%}) had {key}")
 
         print(f"Wrote {len(output_catalog):,d} total records to {output_catalog_path}")
 
-    print("----")
-    if counters["already had adjacent loci"]:
-        print(f"{counters['already had adjacent loci']:,d} out of {counters['total records']:,d} records already had adjacent "
-              f"loci specified in the input catalog, and so were added to the output catalog without modification")
-    else:
-        print("None of the records in the input catalog(s) had adjacent loci specified")
-    print(f"Added adjacent loci to {counters['added adjacent loci']:,d} out of {counters['total records']:,d} records ("
-          f"{counters['added adjacent loci'] / counters['total records']:.1%})")
-
-    for key, value in sorted(counters.items(), key=lambda x: x[0]):
-        if key.startswith("total") or key in {"already had adjacent loci", "added adjacent loci"}:
-            continue
-        if "adjacent repeats count" in key:
-            label = "reference regions"
-        elif "spacer" in key:
-            label = "spacers"
-        else:
-            continue
-
-        total = counters[f"total {label}"]
-        print(f"{value:8,d} out of {total:8,d} {label} ({value / total:6.1%}) had {key}")
 
 
 if __name__ == '__main__':
