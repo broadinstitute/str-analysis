df3a3a4451562016fe5c7baa26a20891f13ede16
bw2 <ben.weisburd@gmail.com>
2024-04-05 09:14:54 -0400

    updated docker image

--- setup.py
+++ setup.py
@@ -30,7 +30,7 @@ class PublishCommand(build_py):
 
 setup(
     name='str_analysis',
-    version="1.2.8",
+    version="1.2.9",
     description="Utilities for analyzing short tandem repeats (STRs)",
     install_requires=requirements,
     cmdclass={
