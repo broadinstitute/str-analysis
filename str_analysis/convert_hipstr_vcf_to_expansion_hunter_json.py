36fcf9a69197ab24a735ffaab7b3b662b058c8ca
bw2 <ben.weisburd@gmail.com>
2024-05-01 12:55:42 -0400

    improved parsing of different locus id formats + added handling of start position offsets

--- str_analysis/convert_hipstr_vcf_to_expansion_hunter_json.py
+++ str_analysis/convert_hipstr_vcf_to_expansion_hunter_json.py
@@ -60,11 +60,22 @@ def main():
     p = argparse.ArgumentParser()
     p.add_argument("--sample-id",
                    help="If not specified, the sample id will be parsed from the last column of the vcf header.")
+    p.add_argument("--skip-loci-with-invalid-locus-ids", action="store_true",
+                   help="Locus Ids are expected to have the format 'chrom-start-end-motif'. Skip loci with ids"
+                        "that don't match this format instead of throwing an error")
+    p.add_argument("--skip-hom-ref-loci", action="store_true", help="Filter ou loci that were called as "
+                                                                         "homozygous reference")
+
     p.add_argument("vcf_path", help="HipSTR vcf path(s)")
     args = p.parse_args()
 
     print(f"Processing {args.vcf_path}")
-    locus_results = process_hipstr_vcf(args.vcf_path, sample_id=args.sample_id)
+    locus_results = process_hipstr_vcf(
+        args.vcf_path,
+        sample_id=args.sample_id,
+        skip_hom_ref_loci=args.skip_hom_ref_loci,
+        skip_loci_with_invalid_locus_ids=args.skip_loci_with_invalid_locus_ids,
+    )
 
     output_json_path = re.sub(".vcf(.gz)?$", "", args.vcf_path) + ".json"
     print(f"Writing {len(locus_results['LocusResults']):,d} loci to {output_json_path}")
@@ -72,7 +83,7 @@ def main():
         json.dump(locus_results, f, indent=4)
 
 
-def process_hipstr_vcf(vcf_path, sample_id=None):
+def process_hipstr_vcf(vcf_path, sample_id=None, skip_hom_ref_loci=False, skip_loci_with_invalid_locus_ids=False):
     locus_results = {
         "LocusResults": {},
         "SampleParameters": {
@@ -84,6 +95,8 @@ def process_hipstr_vcf(vcf_path, sample_id=None):
     fopen = gzip.open if vcf_path.endswith("gz") else open
     with fopen(vcf_path, "rt") as vcf:
         line_counter = 0
+        locus_pos_adjust_counter = 0
+
         for line in vcf:
             if line.startswith("#"):
                 if line.startswith("#CHROM"):
@@ -107,10 +120,23 @@ def process_hipstr_vcf(vcf_path, sample_id=None):
             if genotype == ".":
                 continue   # no genotype
 
+            if skip_hom_ref_loci and genotype == "0/0":
+                continue
+
             chrom = fields[0]
-            start_1based = int(fields[1])
+            pos = int(fields[1])
             locus_id = fields[2]
-            _, _, _, repeat_unit = locus_id.split("-")
+            locus_id_fields = locus_id.split("-")
+            if len(locus_id_fields) >= 4 and len(set(locus_id_fields[3]) - set("ACGTRN")) == 0:
+                repeat_unit = locus_id_fields[3]
+            else:
+                if skip_loci_with_invalid_locus_ids:
+                    continue
+                else:
+                    raise ValueError(
+                        f"VCF row #{line_counter}: Unable to parse repeat unit from locus id '{locus_id}'. "
+                        f"Expected locus id to have the format 'chrom-start-end-motif'. Instead, found '{locus_id}'")
+
             ref = fields[3]
             alts = fields[4].split(",")
             alleles = [ref] + alts
@@ -128,7 +154,10 @@ def process_hipstr_vcf(vcf_path, sample_id=None):
                     genotype_dict[key] = float(value.strip('"'))
                 except ValueError:
                     continue
+
+            start_1based = int(info_dict["START"])
             end_1based = int(info_dict["END"])
+            period = int(info_dict["PERIOD"])
 
             try:
                 left_allele, right_allele = map(int, genotype.split("/"))
@@ -138,6 +167,18 @@ def process_hipstr_vcf(vcf_path, sample_id=None):
                     short_allele, long_allele = allele1, allele2
                 else:
                     short_allele, long_allele = allele2, allele1
+
+                if pos < start_1based:
+                    if start_1based - pos >= len(short_allele):
+                        raise ValueError(f"start_1based ({start_1based:,d}) - pos ({pos:,d}) >= len(short_allele) ({len(short_allele):,d}). The short allele is '{short_allele}'")
+                    if start_1based - pos >= len(long_allele):
+                        raise ValueError(f"start_1based ({start_1based:,d}) - pos ({pos:,d}) >= len(long_allele) ({len(long_allele):,d}). The long allele is '{long_allele}'")
+                    locus_pos_adjust_counter += 1
+                    print(f"WARNING: {locus_id} VCF row has pos < start_1based: {pos:,d} < {start_1based:,d}. Diff: {start_1based - pos}bp. "
+                          f"Trimming alleles: {short_allele} => {short_allele[start_1based - pos:]} and "
+                          f"{long_allele} => {long_allele[start_1based - pos:]}")
+                    short_allele = short_allele[start_1based - pos:]
+                    long_allele = long_allele[start_1based - pos:]
 
                 num_repeats1 = int(len(short_allele)/len(repeat_unit))
                 num_repeats2 = int(len(long_allele)/len(repeat_unit))
@@ -165,6 +206,9 @@ def process_hipstr_vcf(vcf_path, sample_id=None):
                 print(f"Error on vcf record #{line_counter}: {e}")
                 print(line)
                 print(genotype_dict)
+
+        if locus_pos_adjust_counter:
+            print(f"Adjusted the start position of {locus_pos_adjust_counter:,d} out of {line_counter:,d} ({locus_pos_adjust_counter/line_counter:.2%}) loci.")
 
     return locus_results
 
