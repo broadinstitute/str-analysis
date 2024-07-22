ce5e6a3af80766f43137f29f88be9d39390cc690
bw2 <ben.weisburd@gmail.com>
2024-07-22 17:34:47 -0400

    Added get_variant_catalog_iterator(..) method

--- str_analysis/utils/eh_catalog_utils.py
+++ str_analysis/utils/eh_catalog_utils.py
@@ -1,5 +1,9 @@
+import ijson
+import re
+from str_analysis.utils.file_utils import open_file
 from str_analysis.utils.misc_utils import parse_interval
 
+ACGT_REGEX = re.compile("^[ACGT]+$", re.IGNORECASE)
 
 def parse_motifs_from_locus_structure(locus_structure):
     """Takes an ExpansionHunter LocusStructure like "(CAG)*AGAC(GCC)+" and returns a list of motifs ["CAG", "GCC"]"""
@@ -28,3 +32,39 @@ def convert_json_records_to_bed_format_tuples(json_records):
         for reference_region, motif in zip(reference_regions, motifs):
             chrom, start_0based, end_1based = parse_interval(reference_region)
             yield chrom, start_0based, end_1based, motif, f"{(end_1based - start_0based) / len(motif):0.1f}"
+
+
+def get_variant_catalog_iterator(variant_catalog_json_or_bed):
+    """Takes the path of a JSON or BED file and returns an iterator over variant catalog records parsed from that file.
+
+    Args:
+        variant_catalog_json_or_bed (str): path to a JSON or BED file containing variant catalog records
+
+    Yields:
+        dict: a variant catalog record parsed from the file
+    """
+
+    if ".json" in variant_catalog_json_or_bed:
+        with open_file(variant_catalog_json_or_bed, is_text_file=True) as f:
+            for record in ijson.items(f, "item"):
+                yield record
+    else:
+        with open_file(variant_catalog_json_or_bed, is_text_file=True) as input_variant_catalog:
+            for line in input_variant_catalog:
+                fields = line.strip().split("\t")
+                unmodified_chrom = fields[0]
+                chrom = unmodified_chrom.replace("chr", "")
+                start_0based = int(fields[1])
+                end_1based = int(fields[2])
+                motif = fields[3].strip("()*+").upper()
+                if not ACGT_REGEX.match(motif):
+                    print(f"WARNING: skipping line with invalid motif: {line.strip()}")
+                    continue
+
+                record = {
+                    "LocusId": f"{chrom}-{start_0based + 1}-{end_1based}-{motif}",
+                    "ReferenceRegion": f"{unmodified_chrom}:{start_0based}-{end_1based}",
+                    "LocusStructure": f"({motif})*",
+                    "VariantType": "Repeat",
+                }
+                yield record
