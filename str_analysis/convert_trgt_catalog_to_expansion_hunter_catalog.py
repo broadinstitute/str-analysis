51c6f03de760895b0cba5b42436c6a18e17ef3f2
bw2 <ben.weisburd@gmail.com>
2024-08-05 11:59:35 -0400

    simplified handling of single-motif locus definitions

--- str_analysis/convert_trgt_catalog_to_expansion_hunter_catalog.py
+++ str_analysis/convert_trgt_catalog_to_expansion_hunter_catalog.py
@@ -20,6 +20,7 @@ def main():
     p.add_argument("-r", "--reference-fasta", required=True, help="Reference FASTA file path")
     p.add_argument("-o", "--output-file", help="ExansionHunter variant catalog JSON output file path")
     p.add_argument("-v", "--verbose", action="store_true")
+    p.add_argument("--set-locus-id", action="store_true", help="Ignore the ID field in the TRGT catalog and set the locus ID to {chrom}-{start}-{end}-{motif}")
     p.add_argument("--show-progress-bar", action="store_true", help="Show a progress bar")
     p.add_argument("trgt_bed_path", help="path of the TRGT repeat catalog BED file")
     args = p.parse_args()
@@ -30,10 +31,14 @@ def main():
     if not os.path.isfile(args.trgt_bed_path):
         p.error(f"{args.trgt_bed_path} file not found")
 
-    process_variant_catalog(args.trgt_bed_path, args.reference_fasta, args.output_file, verbose=args.verbose, show_progress_bar=args.show_progress_bar)
+    process_variant_catalog(args.trgt_bed_path, args.reference_fasta, args.output_file,
+                            set_locus_id=args.set_locus_id,
+                            verbose=args.verbose,
+                            show_progress_bar=args.show_progress_bar)
 
 
-def process_variant_catalog(trgt_bed_path_path, reference_fasta_path, output_file_path, verbose=False, show_progress_bar=False):
+def process_variant_catalog(trgt_bed_path_path, reference_fasta_path, output_file_path, set_locus_id=False,
+                            verbose=False, show_progress_bar=False):
 
     fasta_obj = pyfaidx.Fasta(reference_fasta_path, one_based_attributes=False, as_raw=True)
 
@@ -57,30 +62,53 @@ def process_variant_catalog(trgt_bed_path_path, reference_fasta_path, output_fil
             info_fields = fields[3]  # Example: ID=chr1_153777445_153777638;MOTIFS=GAAGAGGAAGTGAGA,GGGGACA;STRUC=GCAGGGAAGGACTGTGTCAGTCCAG(GAAGAGGAAGTGAGA)n(GGGGACA)nGCACGGCTTCCTGACGGTGTGACTC
             info_fields_dict = {}
             for key_value in info_fields.split(";"):
-                if len(key_value.split("=")) != 2:
+                key_value = key_value.split("=")
+                if len(key_value) != 2:
                     print(f"WARNING: skipping invalid key-value pair '{key_value}' in line {fields}")
                     continue
-                key, value = key_value.split("=")
+                key, value = key_value
                 info_fields_dict[key] = value
-            locus_id = info_fields_dict.get("ID", f"{chrom}-{start_0based}-{end_1based}")
+
             locus_structure = info_fields_dict.get("STRUC", "")
             motifs_from_locus_structure = parse_motifs_from_locus_structure(locus_structure)
             motifs = info_fields_dict.get("MOTIFS")
-            motifs = motifs.split(",") if motifs else motifs_from_locus_structure
-            if tuple(motifs) != tuple(motifs_from_locus_structure):
+            if not motifs:
+                raise ValueError(f"MOTIFS field not found in row #{i + 1}: {fields}")
+            motifs = motifs.split(",")
+            if set(motifs) != set(motifs_from_locus_structure):
                 print(f"WARNING: MOTIFS {tuple(motifs)} != motifs in locus structure {tuple(motifs_from_locus_structure)} in row #{i + 1}: {fields}")
                 continue
 
+            motifs = motifs_from_locus_structure
             for motif in motifs:
                 if not motif or (len(set(motif) - set("ACGTN")) > 0):
                     raise ValueError(f"Invalid repeat unit in row #{i + 1}: {repeat_unit}. Line: {row}")
 
-            # TODO how do you convert to intervals
             counter["total input loci"] += 1
 
             # get reference sequence
             if chrom not in fasta_obj:
                 raise ValueError(f"Chromosome '{chrom}' not found in reference FASTA file which has chromosomes {list(fasta_obj.keys())}")
+
+            if len(motifs) == 1:
+                motif = motifs[0]
+                if set_locus_id:
+                    locus_id = f"{chrom}-{start_0based}-{end_1based}-{motif}"
+                else:
+                    locus_id = info_fields_dict.get("ID")
+                    if not locus_id:
+                        raise ValueError(f"ID field not found in row #{i + 1}: {fields}. One work-around is to rerun with --set-locus-id")
+
+                if verbose:
+                    print(f"Adding locus {locus_id} with motif {motif} and reference region {chrom}:{start_0based}-{end_1based}")
+                output_json_records.append({
+                    "LocusId": locus_id,
+                    "ReferenceRegion": f"{chrom}:{start_0based}-{end_1based}",
+                    "LocusStructure": f"({motif})*",
+                    "VariantType": "Repeat",
+                })
+
+                continue
 
             unprocessed_reference_sequence = fasta_obj[chrom][start_0based:end_1based]
             unprocessed_locus_structure = locus_structure
@@ -119,7 +147,14 @@ def process_variant_catalog(trgt_bed_path_path, reference_fasta_path, output_fil
 
                 if current_start_coord_0based != current_end_coord and repeat_counter > 1:
 
-                    current_locus_id = f"{locus_id}-{motif}" if len(motifs) > 1 else locus_id
+                    if set_locus_id:
+                        current_locus_id = f"{chrom}-{current_start_coord_0based}-{current_end_coord}"
+                    else:
+                        current_locus_id = info_fields_dict.get("ID")
+                        if not current_locus_id:
+                            raise ValueError(f"ID field not found in row #{i + 1}: {fields}. One work-around is to rerun with --set-locus-id")
+
+                    current_locus_id = f"{current_locus_id}-{motif}"
 
                     added_locus=True
                     if verbose:
@@ -137,14 +172,17 @@ def process_variant_catalog(trgt_bed_path_path, reference_fasta_path, output_fil
             if not added_locus:
                 counter["skipped"] += 1
                 if verbose:
-                    print(f"WARNING: No output loci written for {locus_id}. This is often because the reference repeat sequence contains interruptions")
+                    print(f"WARNING: Skipped {locus_id} due to issues with parsing the reference sequence. This typically happens when reference repeat sequence contains interruptions. {motifs}, {fields}")
 
     print(f"Writing {len(output_json_records):,d} records to {output_file_path}")
     fopen = gzip.open if output_file_path.endswith("gz") else open
     with fopen(output_file_path, "wt") as f:
         json.dump(output_json_records, f, indent=4, ignore_nan=True)
 
-    print("Stats:", pformat(dict(counter)))
+    print("Stats:")
+    for key, value in sorted(counter.items(), key=lambda x: x[1], reverse=True):
+        print(f"{value:10,d} loci {key}")
+
     print(f"Wrote {len(output_json_records):,d} records to {output_file_path}")
 
 
