7a7c30a8de92fb1d6e08c6c85b19dc640db04100
bw2 <ben.weisburd@gmail.com>
2024-08-14 14:58:12 -0400

    minor updates to output stats

--- str_analysis/compute_catalog_stats.py
+++ str_analysis/compute_catalog_stats.py
@@ -99,8 +99,9 @@ def compute_catalog_stats(catalog_name, records, verbose=False, show_progress_ba
             max_motif_size = max(max_motif_size, motif_size)
             min_locus_size = min(min_locus_size, locus_size)
             max_locus_size = max(max_locus_size, locus_size)
-            min_num_repeats_in_locus = min(min_num_repeats_in_locus, int(locus_size / motif_size))
-            max_num_repeats_in_locus = max(max_num_repeats_in_locus, int(locus_size / motif_size))
+            motif_sizes_per_locus_size = int(locus_size / motif_size)
+            min_num_repeats_in_locus = min(min_num_repeats_in_locus, motif_sizes_per_locus_size)
+            max_num_repeats_in_locus = max(max_num_repeats_in_locus, motif_sizes_per_locus_size)
 
             counters["total_base_pairs_spanned_by_all_loci"] += locus_size
             if locus_size == max_locus_size:
@@ -131,11 +132,13 @@ def compute_catalog_stats(catalog_name, records, verbose=False, show_progress_ba
             motif_size_bin = f"{motif_size}bp" if motif_size <= 6 else "7-24bp" if motif_size <= 24 else "25+bp"
             counters[f"motif_size:{motif_size_bin}"] += 1
 
-            num_repeats_in_ref = int(locus_size / motif_size)
-            num_repeats_in_ref_bin = f"{num_repeats_in_ref}x" if num_repeats_in_ref <= 9 else "10-15x" if num_repeats_in_ref <= 15 else "16-25x" if num_repeats_in_ref <= 25 else "26-35x" if num_repeats_in_ref <= 35 else "36-50x" if num_repeats_in_ref <= 50 else "51+x"
-            #if num_repeats_in_ref <= 3:
-            #    print(num_repeats_in_ref_bin, record["LocusId"], reference_region, motif)
-            counters[f"num_repeats_in_ref:{num_repeats_in_ref_bin}"] += 1
+            motif_sizes_per_locus_size_bin = f"{motif_sizes_per_locus_size}x" if motif_sizes_per_locus_size <= 9 else "10-15x" if motif_sizes_per_locus_size <= 15 else "16-25x" if motif_sizes_per_locus_size <= 25 else "26-35x" if motif_sizes_per_locus_size <= 35 else "36-50x" if motif_sizes_per_locus_size <= 50 else "51+x"
+            counters[f"motif_sizes_per_locus_size:{motif_sizes_per_locus_size_bin}"] += 1
+
+            motif_sizes_per_locus_size_detailed_bin = f"{motif_sizes_per_locus_size}x" if motif_sizes_per_locus_size <= 20 else "21-25x" if motif_sizes_per_locus_size <= 25 else "26-30x" if motif_sizes_per_locus_size <= 30 else "31-35x" if motif_sizes_per_locus_size <= 35 else "36-50x" if motif_sizes_per_locus_size <= 50 else "51+x"
+            #if motif_sizes_per_locus_size <= 3:
+            #    print(motif_sizes_per_locus_size_bin, record["LocusId"], reference_region, motif)
+            counters[f"motif_sizes_per_locus_size_detailed:{motif_sizes_per_locus_size_detailed_bin}"] += 1
 
             if fraction_pure_bases is not None:
                 fraction_pure_bases_bin = round(int(fraction_pure_repeats*10)/10, 1)
@@ -163,6 +166,12 @@ def compute_catalog_stats(catalog_name, records, verbose=False, show_progress_ba
     print("")
     print(f"Stats for {catalog_name}:")
     print(f"   {counters['total']:10,d} total loci")
+    if counters["total_repeat_intervals"] == 0:
+        return {
+            "catalog": catalog_name,
+            "total": f"{counters['total_repeat_intervals']:,d}",
+        }
+
     total_genome_size = 3_088_286_401  # in hg38, including chrX, chrY, chrM
     print(f"   {counters['total_base_pairs_spanned_by_all_loci']:10,d} base pairs spanned by all loci ({counters['total_base_pairs_spanned_by_all_loci']/total_genome_size:0.3%} of the genome)")
     print(f"   {counters['loci_with_adjacent_repeats']:10,d} out of {counters['total']:10,d} ({counters['loci_with_adjacent_repeats']/counters['total']:6.1%}) loci define adjacent repeats")
@@ -186,13 +195,13 @@ def compute_catalog_stats(catalog_name, records, verbose=False, show_progress_ba
     print(f"   Locus size range: {min_locus_size}-{max_locus_size}bp")
     print(f"   Num repeats range: {min_num_repeats_in_locus}-{max_num_repeats_in_locus}x repeats")
     print("")
-    print(f"   Maximum locus size = {max_locus_size}bp               @ {max_locus_size_reference_region} ({max_locus_size_motif})")
+    print(f"   Maximum locus size = {max_locus_size:5d}bp             @ {max_locus_size_reference_region} ({max_locus_size_motif})")
     if min_fraction_pure_bases_motif is not None:
-        print(f"   Minimum fraction pure bases = {min_fraction_pure_bases:.2f}      @ {min_fraction_pure_bases_reference_region} ({min_fraction_pure_bases_motif})")
+        print(f"   Minimum fraction pure bases = {min_fraction_pure_bases:5.2f}      @ {min_fraction_pure_bases_reference_region} ({min_fraction_pure_bases_motif})")
     if min_fraction_pure_repeats_motif is not None:
-        print(f"   Minimum fraction pure repeats = {min_fraction_pure_repeats:.2f}    @ {min_fraction_pure_repeats_reference_region} ({min_fraction_pure_repeats_motif})")
+        print(f"   Minimum fraction pure repeats = {min_fraction_pure_repeats:5.2f}    @ {min_fraction_pure_repeats_reference_region} ({min_fraction_pure_repeats_motif})")
     if min_overall_mappability_motif is not None:
-        print(f"   Minimum overall mappability = {min_overall_mappability:.2f}       @ {min_overall_mappability_reference_region} ({min_overall_mappability_motif})")
+        print(f"   Minimum overall mappability = {min_overall_mappability:5.2f}       @ {min_overall_mappability_reference_region} ({min_overall_mappability_motif})")
     print("")
     print(f"          chrX: {counters['chrX']:10,d} out of {counters['total_repeat_intervals']:10,d} ({counters['chrX']/counters['total_repeat_intervals']:6.1%}) repeat intervals")
     print(f"          chrY: {counters['chrY']:10,d} out of {counters['total_repeat_intervals']:10,d} ({counters['chrY']/counters['total_repeat_intervals']:6.1%}) repeat intervals")
@@ -204,8 +213,8 @@ def compute_catalog_stats(catalog_name, records, verbose=False, show_progress_ba
         print(f"   {motif_size_bin:>10s}: {counters[f'motif_size:{motif_size_bin}']:10,d} out of {counters['total_repeat_intervals']:10,d} ({counters[f'motif_size:{motif_size_bin}']/counters['total_repeat_intervals']:6.1%}) repeat intervals")
     print("")
     print("Num repeats in reference:")
-    for num_repeats_in_ref_bin in "1x", "2x", "3x", "4x", "5x", "6x", "7x", "8x", "9x", "10-15x", "16-25x", "26-35x", "36-50x", "51+x":
-        print(f"   {num_repeats_in_ref_bin:>10s}: {counters[f'num_repeats_in_ref:{num_repeats_in_ref_bin}']:10,d} out of {counters['total_repeat_intervals']:10,d} ({counters[f'num_repeats_in_ref:{num_repeats_in_ref_bin}']/counters['total_repeat_intervals']:6.1%}) repeat intervals")
+    for motif_sizes_per_locus_size_bin in "1x", "2x", "3x", "4x", "5x", "6x", "7x", "8x", "9x", "10-15x", "16-25x", "26-35x", "36-50x", "51+x":
+        print(f"   {motif_sizes_per_locus_size_bin:>10s}: {counters[f'motif_sizes_per_locus_size:{motif_sizes_per_locus_size_bin}']:10,d} out of {counters['total_repeat_intervals']:10,d} ({counters[f'motif_sizes_per_locus_size:{motif_sizes_per_locus_size_bin}']/counters['total_repeat_intervals']:6.1%}) repeat intervals")
 
     if min_fraction_pure_bases_motif is not None:
         print("")
@@ -221,7 +230,7 @@ def compute_catalog_stats(catalog_name, records, verbose=False, show_progress_ba
 
     result = {
         "catalog": catalog_name,
-        "total": f"{counters['total_repeat_intervals']:,d}",
+        "total": counters['total_repeat_intervals'],
         "count_chrX": counters['chrX'],
         "count_chrY": counters['chrY'],
         "count_chrM": counters["chrM"],
@@ -229,6 +238,7 @@ def compute_catalog_stats(catalog_name, records, verbose=False, show_progress_ba
         "locus_size_range": f"{min_locus_size}-{max_locus_size}bp",
         "num_repeats_range": f"{min_num_repeats_in_locus}-{max_num_repeats_in_locus}x repeats",
         "percent_homopolymers": "%0.1f%%" % (100 * counters["motif_size:1bp"] / counters["total_repeat_intervals"]),
+        "percent_2bp_motifs": "%0.1f%%" % (100 * counters["motif_size:1bp"] / counters["total_repeat_intervals"]),
         "percent_2bp_motifs": "%0.1f%%" % (100 * counters["motif_size:2bp"] / counters["total_repeat_intervals"]),
         "percent_3bp motifs": "%0.1f%%" % (100 * counters["motif_size:3bp"] / counters["total_repeat_intervals"]),
         "percent_4bp_motifs": "%0.1f%%" % (100 * counters["motif_size:4bp"] / counters["total_repeat_intervals"]),
@@ -239,12 +249,15 @@ def compute_catalog_stats(catalog_name, records, verbose=False, show_progress_ba
         "percent_trimmed": "%0.1f%%" % (100 * counters["trimmed"] / counters["total_repeat_intervals"]),
         "percent_overlapping": "%0.1f%%" % (100 * len(overlapping_intervals) / counters['total_repeat_intervals']),
         "count_homopolymers": counters["motif_size:1bp"],
+        "count_1bp_motifs": counters["motif_size:1bp"],
         "count_2bp_motifs": counters["motif_size:2bp"],
         "count_3bp motifs":  counters["motif_size:3bp"],
         "count_4bp_motifs":  counters["motif_size:4bp"],
         "count_5bp_motifs":  counters["motif_size:5bp"],
         "count_6bp_motifs":  counters["motif_size:6bp"],
         "count_7+bp_motifs": counters["motif_size:7-24bp"] + counters["motif_size:25+bp"],
+        "count_7-24bp_motifs": counters["motif_size:7-24bp"],
+        "count_25+bp_motifs": counters["motif_size:25+bp"],
         "count_pure_repeats": counters[f"fraction_pure_bases:1.0"],
         "count_trimmed": counters["trimmed"],
         "count_overlapping": len(overlapping_intervals),
@@ -253,27 +266,13 @@ def compute_catalog_stats(catalog_name, records, verbose=False, show_progress_ba
         "min_locus_size": min_locus_size,
         "max_locus_size": max_locus_size,
     }
+    for motif_sizes_per_locus_size_detailed_bin in (
+        "0x", "1x", "2x", "3x", "4x", "5x", "6x", "7x", "8x", "9x", "10x",
+        "11x", "12x", "13x", "14x", "15x", "16x", "17x", "18x", "19x", "20x",
+        "21-25x", "26-30x", "31-35x", "36-50x", "51+x"
+    ):
+        result[f"motif_sizes_per_locus_size:{motif_sizes_per_locus_size_detailed_bin}"] = counters[f"motif_sizes_per_locus_size_detailed:{motif_sizes_per_locus_size_detailed_bin}"]
 
-    """
-    "min_fraction_pure_bases": min_fraction_pure_bases,
-    "min_fraction_pure_bases_reference_region": min_fraction_pure_bases_reference_region,
-    "min_fraction_pure_bases_motif": min_fraction_pure_bases_motif,
-    "min_fraction_pure_repeats": min_fraction_pure_repeats,
-    "min_fraction_pure_repeats_reference_region": min_fraction_pure_repeats_reference_region,
-    "min_fraction_pure_repeats_motif": min_fraction_pure_repeats_motif,
-    "motif_size_distribution": {
-        "1bp": counters["motif_size:1bp"],
-        "2bp": counters["motif_size:2bp"],
-        "3bp": counters["motif_size:3bp"],
-        "4bp": counters["motif_size:4bp"],
-        "5bp": counters["motif_size:5bp"],
-        "6bp": counters["motif_size:6bp"],
-        "7-24bp": counters["motif_size:7-24bp"],
-        "25+bp": counters["motif_size:25+bp"],
-    },
-    "fraction_pure_bases_distribution": {},
-    "mappability_distribution": {},
-    """
 
     return result
 
