34343fb97455d1790da1d406e19ddaf044decdc1
bw2 <ben.weisburd@gmail.com>
2025-07-04 19:53:08 -0400

    continuing to implement str_analysis/combine_variant_tables_from_filter_vcf_to_STR.py

--- str_analysis/combine_variant_tables_from_filter_vcf_to_STR.py
+++ str_analysis/combine_variant_tables_from_filter_vcf_to_STR.py
@@ -105,6 +105,7 @@ SCHEMA_WITH_INT["NumRepeatsLongAllele"] = pl.Int32
 #SAMPLES_TABLE_CHECKPOINT_FILENAME = "temp_df_with_samples.tsv.gz"
 
 
+
 def parse_input_tsv(sample_id, input_tsv, exclude_homopolymers = False, discard_impure_genotypes = False):
 
     # Read with polars
@@ -139,23 +140,152 @@ def parse_input_tsv(sample_id, input_tsv, exclude_homopolymers = False, discard_
     df = df.rename(rename_dict)
 
     # sort by chrom, start, end
-    df = df.sort(["Chrom", "Start1Based", "End1Based"])
+    #df = df.sort(["Chrom", "Start1Based", "End1Based"])
 
     return df
 
-def merge_overlapping_loci(df, verbose=True):
+
+def merge_two_rows(keep_row, drop_row, locus_id_to_sample_ids_with_non_missing_genotypes, merge_per_sample_columns=False, verbose=False):
+    # transfer all values from the "drop_row" to the "keep_row"
+    keep_row = keep_row.copy()
+
+    # update the ShortAllelesList and LongAllelesList. The allele_size should always be non-negative anyway 
+    # since keep_row is the one with the larger reference interval, but use max(0, ..) to be safe.
+    num_repeats_offset = keep_row["NumRepeatsInReference"] - drop_row["NumRepeatsInReference"]
+
+    new_short_allele_list = []
+    new_long_allele_list = []
+    for keep_row_short_allele, keep_row_long_allele, drop_row_short_allele, drop_row_long_allele in zip(
+        keep_row["ShortAllelesList"], keep_row["LongAllelesList"], drop_row["ShortAllelesList"], drop_row["LongAllelesList"]
+    ):
+        drop_row_placeholders = {None, drop_row["NumRepeatsInReference"]}
+        keep_row_placeholders = {None, keep_row["NumRepeatsInReference"]}
+
+        # at each position in the lists, either keep the allele from the keep_row or the allele from the drop_row
+        if (drop_row_short_allele is None and drop_row_long_allele is None) or (drop_row_short_allele == drop_row["NumRepeatsInReference"] and drop_row_long_allele == drop_row["NumRepeatsInReference"]):
+            new_short_allele_list.append(keep_row_short_allele)
+            new_long_allele_list.append(keep_row_long_allele)
+        elif (keep_row_short_allele is None and keep_row_long_allele is None) or (keep_row_short_allele == keep_row["NumRepeatsInReference"] and keep_row_long_allele == keep_row["NumRepeatsInReference"]):
+            new_short_allele_list.append(drop_row_short_allele)
+            new_long_allele_list.append(drop_row_long_allele)
+        elif keep_row_short_allele not in keep_row_placeholders and drop_row_short_allele not in drop_row_placeholders:
+            raise ValueError(f"keep_row_short_allele {keep_row_short_allele} and drop_row_short_allele {drop_row_short_allele} are both non-placeholders")
+
+    #if keep_row["LocusId"] == "9-133392583-133392639-TGGA":
+    #    print("keep_row ShortAllelesList, LongAllelesList", keep_row["ShortAllelesList"], keep_row["LongAllelesList"])
+    #    print("drop_row ShortAllelesList, LongAllelesList", drop_row["ShortAllelesList"], drop_row["LongAllelesList"])
+
+    #    print("new_row new_short_allele_list", new_short_allele_list)
+    #    print("new_row new_long_allele_list", new_long_allele_list)
+
+    assert len(keep_row["ShortAllelesList"]) == len(keep_row["LongAllelesList"])
+    assert len(new_short_allele_list) == len(keep_row["ShortAllelesList"])
+    assert len(new_long_allele_list) == len(keep_row["LongAllelesList"])
+
+    keep_row["ShortAllelesList"] = new_short_allele_list
+    keep_row["LongAllelesList"] = new_long_allele_list
+
+    # update the "locus_id_to_sample_ids_with_non_missing_genotypes" dictionary
+    locus_id_to_sample_ids_with_non_missing_genotypes[keep_row["LocusId"]] |= locus_id_to_sample_ids_with_non_missing_genotypes[drop_row["LocusId"]]
+    del locus_id_to_sample_ids_with_non_missing_genotypes[drop_row["LocusId"]]
+
+    # if they are present, transfer any per-sample NumRepeatsShortAllele or NumRepeatsLongAllele values from the "drop_row" to the "keep_row"
+    if merge_per_sample_columns:
+        all_sample_ids = {
+            c.replace("NumRepeatsShortAllele:", "").replace("NumRepeatsLongAllele:", "") for c in keep_row.keys() if c.startswith("NumRepeatsShortAllele:") or c.startswith("NumRepeatsLongAllele:")
+        }
+
+        drop_row_placeholders = {None, drop_row["NumRepeatsInReference"]}
+        keep_row_placeholders = {None, keep_row["NumRepeatsInReference"]}
+        for sample_id in all_sample_ids:
+            if not (
+                drop_row[f"NumRepeatsShortAllele:{sample_id}"] is None and drop_row[f"NumRepeatsLongAllele:{sample_id}"] is None
+                or
+                drop_row[f"NumRepeatsShortAllele:{sample_id}"] == drop_row["NumRepeatsInReference"] and drop_row[f"NumRepeatsLongAllele:{sample_id}"] == drop_row["NumRepeatsInReference"]
+            ):
+                assert keep_row[f"NumRepeatsShortAllele:{sample_id}"] in keep_row_placeholders and keep_row[f"NumRepeatsLongAllele:{sample_id}"] in keep_row_placeholders, (f"{column_name}\nkeep_row: {keep_row}\ndrop_row:{drop_row}")
+                
+                if keep_row[f"NumRepeatsShortAllele:{sample_id}"] is None and keep_row[f"NumRepeatsLongAllele:{sample_id}"] is None:
+                    keep_row["MissingHaplotypes"] -= 2
+
+                keep_row[f"NumRepeatsShortAllele:{sample_id}"] = drop_row[f"NumRepeatsShortAllele:{sample_id}"] + num_repeats_offset if drop_row[f"NumRepeatsShortAllele:{sample_id}"] is not None else None
+                keep_row[f"NumRepeatsLongAllele:{sample_id}"] = drop_row[f"NumRepeatsLongAllele:{sample_id}"] + num_repeats_offset if drop_row[f"NumRepeatsLongAllele:{sample_id}"] is not None else None
+
+        assert keep_row["MissingHaplotypes"] >= 0, keep_row
+
+
+    if verbose:  #  and row_i["LocusId"] == "19-29537703-29537757-CACCCACCTACTCTCCCG"
+        pure_i = "Pure" if keep_row["IsPureRepeat"] else "Interrupted"
+        pure_i_plus_1 = "Pure" if drop_row["IsPureRepeat"] else "Interrupted"
+        pure_keep_row = "Pure" if keep_row["IsPureRepeat"] else "Interrupted"
+        print(f"Merging rows: {keep_row['LocusId']} ({pure_i}) and {drop_row['LocusId']} ({pure_i_plus_1}), keeping {keep_row['LocusId']} ({pure_keep_row}). Adding {num_repeats_offset}x to dropped")
+        #print(locus_id_to_sample_ids_with_non_missing_genotypes[row_i["LocusId"]] & locus_id_to_sample_ids_with_non_missing_genotypes[row_i_plus_1["LocusId"]])
+
+    return keep_row
+
+
+
+def merge_overlapping_loci(df, locus_id_to_sample_ids_with_non_missing_genotypes, merge_per_sample_columns=False, verbose=True):
 
     # sort by chrom, start, end
     df = df.sort(["Chrom", "Start1Based", "End1Based"])
 
-    # iterate over the loci, checking for overlap between each locus and the next one, and then and create a new table with the merged loci
-    merged_df = pl.DataFrame()
-    for i in range(df.height - 1):
-        if df.row(i)["Chrom"] == df.row(i + 1)["Chrom"] and df.row(i + 1)["Start1Based"] <= df.row(i)["End1Based"]:
-            merged_df = merged_df.append(df.row(i))
-        else:
-            merged_df = merged_df.append(df.row(i))
+    #df_temp = df
+    #df_temp = generate_combined_columns(df_temp)
+    #checkpoint_combined_df(df_temp, temp_table_path="temp_before_merge.tsv.gz")
 
+    # iterate over the loci, checking for overlap between each locus and the next one, and then and create a new table with the merged loci
+    result_rows = []
+    i = 0
+    row_i = df.row(i, named=True)
+    merged_row_counter = 0
+    while i < df.height - 1:
+        row_i_plus_1 = df.row(i + 1, named=True)
+        if (
+            # The two loci overlap by 1 or more motif sizes
+            row_i["Chrom"] == row_i_plus_1["Chrom"] and row_i_plus_1["Start1Based"] + row_i["MotifSize"] <= row_i["End1Based"] 
+            and (
+                # The two loci have the same canonical motif for STRs, or the same-sized motif for VNTRs
+                row_i["CanonicalMotif"] == row_i_plus_1["CanonicalMotif"] or (
+                row_i["MotifSize"] == row_i_plus_1["MotifSize"] and row_i["MotifSize"] > 6)
+            )
+            and (
+                # There is no overlap between the samples that have a genotype at one of the loci and the samples that have a genotype at the other locus
+                0 == len(locus_id_to_sample_ids_with_non_missing_genotypes[row_i["LocusId"]] & locus_id_to_sample_ids_with_non_missing_genotypes[row_i_plus_1["LocusId"]])
+            )
+        ):
+            if row_i["NumRepeatsInReference"] >= row_i_plus_1["NumRepeatsInReference"]:
+                keep_row_i = i
+                drop_row_i = i + 1
+            else:
+                keep_row_i = i + 1
+                drop_row_i = i
+
+            merged_row = merge_two_rows(
+                df.row(keep_row_i, named=True), 
+                df.row(drop_row_i, named=True), 
+                locus_id_to_sample_ids_with_non_missing_genotypes, 
+                merge_per_sample_columns=merge_per_sample_columns, 
+                verbose=verbose)
+
+            merged_row_counter += 1
+            row_i = merged_row   # compare the next row to the merged row
+        else:
+            result_rows.append(row_i)
+            row_i = df.row(i + 1, named=True)
+
+        i += 1
+        
+    # Add the last row if we haven't processed it yet
+    result_rows.append(row_i)
+
+    if result_rows:
+        merged_df = pl.DataFrame(result_rows, schema=df.schema)
+    else:
+        merged_df = pl.DataFrame(schema=df.schema)
+
+    print(f"Merged {merged_row_counter:,d} out of {df.height:,d} ({merged_row_counter/df.height:.1%}) overlapping loci")
+    return merged_df
 
 
 def checkpoint_combined_df(df, temp_table_path=None):
@@ -321,9 +451,11 @@ def fill_in_missing_genotypes(df, sample_id, dipcall_confidence_region_interval_
     return df
 
 
-def generate_summary_stats(combined_df):
+def generate_combined_columns(combined_df):
     
-    def compute_summary_stats(row):
+    def compute_columns(row):
+        assert len(row["ShortAllelesList"]) == len(row["LongAllelesList"]), row
+        
         all_non_null_alleles = []
         all_non_null_alleles.extend([allele_size for allele_size in row["ShortAllelesList"] if allele_size is not None])
         all_non_null_alleles.extend([allele_size for allele_size in row["LongAllelesList"] if allele_size is not None])
@@ -367,7 +499,7 @@ def generate_summary_stats(combined_df):
 
     # Drop intermediate columns "ShortAllelesList" and "LongAllelesList"
     combined_df = combined_df.with_columns(
-        pl.struct(["ShortAllelesList", "LongAllelesList"]).map_elements(compute_summary_stats, return_dtype=pl.Struct([
+        pl.struct(["ShortAllelesList", "LongAllelesList"]).map_elements(compute_columns, return_dtype=pl.Struct([
             pl.Field("ModeAllele", pl.Float64),
             pl.Field("Stdev", pl.Float64),
             pl.Field("Median", pl.Float64),
@@ -386,6 +518,7 @@ def generate_summary_stats(combined_df):
 
 def add_sample_columns(combined_df, sample_id_to_input_tsv, sample_id_to_dipcall_confidence_region_interval_trees, include_per_sample_columns=False, exclude_homopolymers=False, discard_impure_genotypes=False):
     all_allele_size_columns = []
+    locus_id_to_sample_ids_with_non_missing_genotypes = collections.defaultdict(set)  # used later for merging overlapping loci
 
     # Add summary stat columns
     combined_df = combined_df.with_columns(
@@ -420,8 +553,23 @@ def add_sample_columns(combined_df, sample_id_to_input_tsv, sample_id_to_dipcall
             pl.concat_list("ShortAllelesList", f"NumRepeatsShortAllele:{sample_id}").alias("ShortAllelesList"), 
             pl.concat_list("LongAllelesList", f"NumRepeatsLongAllele:{sample_id}").alias("LongAllelesList"), 
             pl.col("TotalHaplotypes").add(2).alias("TotalHaplotypes"),
-            pl.col("MissingHaplotypes").add(pl.col(f"NumRepeatsShortAllele:{sample_id}").is_null() + pl.col(f"NumRepeatsLongAllele:{sample_id}").is_null()).alias("MissingHaplotypes"),
+            pl.col("MissingHaplotypes").add(
+                pl.when(pl.col(f"NumRepeatsShortAllele:{sample_id}").is_null()).then(1).otherwise(0) + 
+                pl.when(pl.col(f"NumRepeatsLongAllele:{sample_id}").is_null()).then(1).otherwise(0)
+            ).alias("MissingHaplotypes"),
         )
+        
+        # Record locus ids where the genotype is not missing and is not HOM-REF (eg. was not set to HOM-REF during the previous step)
+        locus_ids_with_non_missing_genotypes_in_this_sample = df.filter(
+            (pl.col(f"NumRepeatsShortAllele:{sample_id}").is_not_null() & (pl.col(f"NumRepeatsShortAllele:{sample_id}") != pl.col("NumRepeatsInReference")))
+            | 
+            (pl.col(f"NumRepeatsLongAllele:{sample_id}").is_not_null() & (pl.col(f"NumRepeatsLongAllele:{sample_id}") != pl.col("NumRepeatsInReference")))
+        ).select(["LocusId"]).to_series()
+
+        for locus_id in locus_ids_with_non_missing_genotypes_in_this_sample:
+            #if locus_id == "19-29537703-29537757-CACCCACCTACTCTCCCG":
+            #    print(f"19-29537703-29537757-CACCCACCTACTCTCCCG: {sample_id}")
+            locus_id_to_sample_ids_with_non_missing_genotypes[locus_id].add(sample_id)
 
         per_sample_columns = [f"NumRepeatsShortAllele:{sample_id}", f"NumRepeatsLongAllele:{sample_id}"]
         if include_per_sample_columns:
@@ -432,7 +580,11 @@ def add_sample_columns(combined_df, sample_id_to_input_tsv, sample_id_to_dipcall
         else:
             combined_df = combined_df.drop(per_sample_columns)
 
-    
+
+    # Merge overlapping rows
+    combined_df = merge_overlapping_loci(
+        combined_df, locus_id_to_sample_ids_with_non_missing_genotypes, merge_per_sample_columns=include_per_sample_columns)
+
     # Drop rows where TotalHaplotypes == MissingHaplotypes, keeping 10 example LocusIds from the dropped rows
     before = combined_df.height
     all_allele_sizes_are_null_df = combined_df.select(["LocusId", "TotalHaplotypes", "MissingHaplotypes"]).filter(pl.col("TotalHaplotypes") == pl.col("MissingHaplotypes"))
@@ -441,8 +593,8 @@ def add_sample_columns(combined_df, sample_id_to_input_tsv, sample_id_to_dipcall
         print(f"Dropped {before - combined_df.height:,d} rows where all allele sizes are null after filtering by confidence regions:", 
             ", ".join(all_allele_sizes_are_null_df.select(["LocusId"]).head(10).to_series().to_list()))
 
-    print("Generating summary stats")
-    combined_df = generate_summary_stats(combined_df)
+    print("Generating combined allele frequencies and other summary columns")
+    combined_df = generate_combined_columns(combined_df)
 
     # Convert to integer columns
     for c in all_allele_size_columns + ["NumRepeatsInReference", "ModeAllele"]:
@@ -518,8 +670,8 @@ def main():
         args.output_tsv += ".gz"
 
     filename_prefix = args.output_tsv.replace(".tsv.gz", "").replace(".tsv", "")
-    if args.output_stats_tsv:
-        output_stats_tsv = f"{filename_prefix}.stats.tsv"
+    
+    output_stats_tsv = f"{filename_prefix}.stats.tsv" if args.output_stats_tsv else None
 
     table_of_all_loci_path = f"{filename_prefix}.only_loci.tsv.gz"
     if not os.path.exists(table_of_all_loci_path) or args.force:
@@ -559,9 +711,6 @@ def main():
         exclude_homopolymers=args.exclude_homopolymers, 
         discard_impure_genotypes=args.discard_impure_genotypes, 
     )
-
-    # merge overlapping loci
-    #combined_df = merge_overlapping_loci(combined_df)
 
     # Sort by PER_LOCUS_COLUMNS
     combined_df = combined_df.sort(PER_LOCUS_COLUMNS)
