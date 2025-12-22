91bd61a282773493f205ad1b4681c2d4f3470aa3
bw2 <ben.weisburd@gmail.com>
2025-12-22 01:48:06 -0500

    renamed functions

--- str_analysis/utils/find_motif_utils_tests.py
+++ str_analysis/utils/find_motif_utils_tests.py
@@ -1,7 +1,7 @@
 import math
 import unittest
 
-from str_analysis.utils.find_motif_utils import compute_repeat_purity, find_optimal_motif_length, _compute_motif_length_quality, HAMMING_DISTANCE_METRIC
+from str_analysis.utils.find_motif_utils import compute_repeat_purity, find_highest_purity_motif_length, _compute_motif_length_quality, HAMMING_DISTANCE_METRIC
 from utils.canonical_repeat_unit import compute_canonical_motif
 
 
@@ -34,15 +34,15 @@ class Tests(unittest.TestCase):
         self.assertEqual(0.5 - 0.1, quality)
 
 
-    def test_find_optimal_motif_length(self):
-        for expected_optimal_motif, sequences, min_quality_score in (
+    def test_find_highest_purity_motif_length(self):
+        for expected_highest_purity_motif, sequences, min_quality_score in (
             ("CAG", ["CAG"*3, "CAG"*3+"C", "CAG"*3+"CA"], 0.6),
             ("CAGA", ["CAGA"*3, "CAGA"*3+"C", "CAGA"*3+"CA"], 0.5),
             ("CAGAT", ["CAGAT"*3, "CAGAT"*3+"C", "CAGAT"*3+"CA"], 0.5),
             ("CAGATC", ["CAGATC"*30, "CAGATC"*30+"C", "CAGATC"*30+"CA", "TT"+"CAGATC"*30+"CA", "TTTT"+"CAGATC"*30+"CA"], 0.5),
         ):
             for seq in sequences:
-                optimal_motif, optimal_purity, optimal_motif_length_quality_score = find_optimal_motif_length(
+                highest_purity_motif, highest_purity, highest_purity_motif_length_quality_score = find_highest_purity_motif_length(
                     nucleotide_sequence=seq,
                     max_motif_length=len(seq)//2,
                     distance_metric=HAMMING_DISTANCE_METRIC,
@@ -50,9 +50,9 @@ class Tests(unittest.TestCase):
                     allow_partial_repeats=True,
                     verbose=True
                 )
-                self.assertEqual(compute_canonical_motif(optimal_motif), compute_canonical_motif(expected_optimal_motif),
+                self.assertEqual(compute_canonical_motif(highest_purity_motif), compute_canonical_motif(expected_highest_purity_motif),
                                  f"Unexpected optimal motif for sequence {seq}")
-                self.assertGreater(optimal_motif_length_quality_score, min_quality_score,
+                self.assertGreater(highest_purity_motif_length_quality_score, min_quality_score,
                                    f"Unexpected optimal motif length quality score for sequence {seq}")
 
 
