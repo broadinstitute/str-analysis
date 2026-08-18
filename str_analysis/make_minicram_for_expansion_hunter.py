df136ce082b618404c7dd60082c48af699ff45d6
Ben Weisburd <bw2@users.noreply.github.com>
2026-08-18 14:12:07 -0400

    Update make_minicram_for_expansion_hunter.py

--- str_analysis/make_minicram_for_expansion_hunter.py
+++ str_analysis/make_minicram_for_expansion_hunter.py
@@ -1,8 +1,8 @@
 """
 This script takes an ExpansionHunter catalog and a CRAM file path - either local or in a Google Storage bucket (gs://) - and extracts the subset of reads that ExpansionHunter will need to genotype that catalog. 
 It is designed so that ExpansionHunter's outputs when it runs on the extracted minicram will be identical to its outputs when it runs on the full CRAM.
-One benefit of using this script is that it minimizes the number of bytes read from the input CRAM file, which, for CRAM files stored in Nearline storage 
-(as is currently the case with AllOfUs genomes) also minimizes the Nearline access costs. 
+One benefit of using this script is that it minimizes the number of bytes read from the input CRAM file compared to using samtools or other htslib-based tools. For CRAM files stored in Nearline storage 
+(as is currently the case with AllOfUs genomes) this substantially reduces Nearline access costs. 
 
 Example command-lines:
 
